2a160efdd8ad8004594b376f6415c34194fc7d82
Joshua Tanner <mindful.jt@gmail.com>
2021-08-01 15:44:28 +0900

    look at heap

--- embed_words_simple.py
+++ embed_words_simple.py
@@ -1,6 +1,7 @@
 import argparse
 import os
 import logging
+from guppy import hpy
 
 from nlp.parsing import EmbeddingExtractor, reduction_function
 from data.db import DbConnection, Word, WriteBuffer
@@ -18,15 +19,21 @@ def main():
 
     db = DbConnection(args.run + '_sentences')
     db_write = DbConnection(args.run + '_words')
-    write_buffer = WriteBuffer('word', db_write.save_words, buffer_size=1)
+    write_buffer = WriteBuffer('word', db_write.save_words, buffer_size=40000)
     extractor = EmbeddingExtractor(embedding_reducer=reduction_function[args.reduction])
 
     sentence_generator = ((text, ident) for ident, text in db.read_sentences(use_tqdm=True))
-    for doc, ident in extractor.nlp.pipe(sentence_generator, batch_size=50, as_tuples=True):
+    counter = 0
+    h = hpy()
+    for doc, ident in extractor.nlp.pipe(sentence_generator, batch_size=500, as_tuples=True):
         try:
             word_gen = (Word(token.text, token.lemma_, token.pos, ident, embedding)
                         for token, embedding in extractor.get_word_embeddings(doc))
             write_buffer.add_many(word_gen)
+            counter += 1
+            if counter % 10000 == 0:
+                print(h.heap())
+
         except Exception as e:
             logger.error('Failed processing doc')
             print(doc)
